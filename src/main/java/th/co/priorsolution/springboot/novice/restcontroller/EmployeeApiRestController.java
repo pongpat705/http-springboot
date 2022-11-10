c27c1ab07424d3efd1369ea4368d5c9a2b10aa2c
maoz.h <pongpat.phokeed@gmail.com>
2022-11-10 16:38:21 +0700

    jpa+native sql(beginning)

--- src/main/java/th/co/priorsolution/springboot/novice/restcontroller/EmployeeApiRestController.java
+++ src/main/java/th/co/priorsolution/springboot/novice/restcontroller/EmployeeApiRestController.java
@@ -3,7 +3,10 @@ package th.co.priorsolution.springboot.novice.restcontroller;
 import org.springframework.web.bind.annotation.*;
 import th.co.priorsolution.springboot.novice.model.EmployeeModel;
 import th.co.priorsolution.springboot.novice.model.ResponseModel;
+import th.co.priorsolution.springboot.novice.model.nativesql.EmployeeOfficeInfos;
 import th.co.priorsolution.springboot.novice.service.EmployeeService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -29,5 +32,10 @@ public class EmployeeApiRestController {
     @PatchMapping("/employee")
     public ResponseModel<Void> updateEmployee(@RequestBody EmployeeModel employeeModel){
         return this.employeeService.updateEmployee(employeeModel);
+    }
+
+    @GetMapping("/employee/employee-office-infos")
+    public ResponseModel<List<EmployeeOfficeInfos>> getEmployeeOfficeInfo(@RequestBody EmployeeModel employeeModel){
+        return employeeService.getEmployeeOfficeInfos(employeeModel);
     }
 }
--- src/main/java/th/co/priorsolution/springboot/novice/service/EmployeeService.java
+++ src/main/java/th/co/priorsolution/springboot/novice/service/EmployeeService.java
@@ -3,14 +3,15 @@ package th.co.priorsolution.springboot.novice.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import th.co.priorsolution.springboot.novice.component.EmployeeModelTransformComponent;
 import th.co.priorsolution.springboot.novice.component.EmployeeValidatorComponent;
 import th.co.priorsolution.springboot.novice.entity.EmployeeEntity;
 import th.co.priorsolution.springboot.novice.model.EmployeeModel;
 import th.co.priorsolution.springboot.novice.model.ErrorModel;
 import th.co.priorsolution.springboot.novice.model.ResponseModel;
+import th.co.priorsolution.springboot.novice.model.nativesql.EmployeeOfficeInfos;
 import th.co.priorsolution.springboot.novice.repository.EmployeeRepository;
+import th.co.priorsolution.springboot.novice.repository.custom.EmployeeCustomRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,15 +22,17 @@ public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
     private final EmployeeModelTransformComponent employeeModelTransformComponent;
-
     private final EmployeeValidatorComponent employeeValidatorComponent;
+    private final EmployeeCustomRepository employeeCustomRepository;
 
     public EmployeeService(@Qualifier("employeeRepository") EmployeeRepository employeeRepository
             , EmployeeModelTransformComponent employeeModelTransformComponent
-            , EmployeeValidatorComponent employeeValidatorComponent) {
+            , EmployeeValidatorComponent employeeValidatorComponent
+            , EmployeeCustomRepository employeeCustomRepository) {
         this.employeeRepository = employeeRepository;
         this.employeeModelTransformComponent = employeeModelTransformComponent;
         this.employeeValidatorComponent = employeeValidatorComponent;
+        this.employeeCustomRepository = employeeCustomRepository;
     }
 
     public ResponseModel<EmployeeModel> getEmployeeByNumber(String number){
@@ -113,6 +116,27 @@ public class EmployeeService {
             }
         } catch (Exception e) {
             log.info("getEmployeeByNumber error {}",e.getMessage());
+            result.setStatus(500);
+            result.setDescription(e.getMessage());
+        }
+
+        return result;
+    }
+
+
+    public ResponseModel<List<EmployeeOfficeInfos>> getEmployeeOfficeInfos(EmployeeModel employeeModel){
+        ResponseModel<List<EmployeeOfficeInfos>> result = new ResponseModel<>();
+        result.setStatus(404);
+        result.setDescription("EmployeeOfficeInfos not found");
+
+        log.info("getEmployeeOfficeInfos {}", employeeModel);
+        try{
+            List<EmployeeOfficeInfos> employeeOfficeInfos = this.employeeCustomRepository.getEmployInfos(employeeModel);
+            result.setData(employeeOfficeInfos);
+            result.setStatus(200);
+            result.setDescription("");
+        } catch (Exception e) {
+            log.info("getEmployeeOfficeInfos error {}",e.getMessage());
             result.setStatus(500);
             result.setDescription(e.getMessage());
         }
